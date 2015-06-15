639f7e1a237aaca7a22f32263cae3e0704ebea02
clintonjnelson <clintonjnelson@live.com>
2015-06-15 08:58:29 -0700

    add passport & strategy into server

--- server.js
+++ server.js
@@ -2,19 +2,26 @@
 
 var express  = require('express' );
 var mongoose = require('mongoose');
+var passport = require('passport');
 var app      = express();
 
 // Routers
-var usersRouter = express.Router();
+var usersRouter  = express.Router();
+var oauth2Router = express.Router();
 
 // Connect DB
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/hippo_dev');
 
+// Initialize passport
+app.use(passport.initialize());
+
 // Load routers
-require('./routes/users_routes.js')(usersRouter);
+require('./routes/users_routes.js' )(usersRouter           );
+require('./routes/oauth2_routes.js')(oauth2Router, passport);
 
 // Assign Routers
-app.use(usersRouter);
+app.use(oauth2Router);
+app.use(usersRouter );
 
 // Start Server (default port: 3000)
 app.listen(process.env.PORT || 3000, function() {
