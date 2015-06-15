6dbac908d143ae6eccc1c094acc9adf4a9e00e28
Eeshan Kumar <e3.kumar@gmail.com>
2015-06-15 15:32:09 -0700

    add facebook request to generate friends on user oAuth success

--- models/User.js
+++ models/User.js
@@ -3,9 +3,10 @@
 var mongoose = require('mongoose');
 
 var UserSchema = mongoose.Schema({
-  eat:         { type: Number                              },
-  facebook_id: { type: String, required: true, unique: true},
-  friends:     { type: Array                               }
+  eat:          { type: Number                              },
+  access_token: {type: String                               },
+  facebook_id:  { type: String, required: true, unique: true},
+  friends:      { type: Array                               }
 });
 
 module.exports = mongoose.model('User', UserSchema);
