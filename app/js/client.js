8e89ffd36cb982f744c76838f3c98c66c9aba6ab
clintonjnelson <clintonjnelson@live.com>
2015-06-15 14:40:20 -0700

    add basic routes

--- app/js/client.js
+++ app/js/client.js
@@ -1,7 +1,32 @@
 'use strict';
 
 require('angular/angular');
+require('angular-route'  );
 
-var hippoApp = angular.module('hippoApp', []);
+var hippoApp = angular.module('hippoApp', ['ngRoute']);
 
+// Services
+
+
+// Controllers
 require('./cards/controllers/cards_controller')(hippoApp);
+
+// Directives
+
+
+// Custom View Routes
+hippoApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/learn', {
+      templateUrl: 'templates/views/card_view.html',
+      controller:  'cards_controller'
+    })
+    .when('/login', {
+      templateUrl: 'templates/views/login.html',
+      controller:  'authController'
+    })
+    .when('/', {
+      redirectTo: '/login'
+    })
+    .otherwise({ redirectTo: '/login' })
+}]);
