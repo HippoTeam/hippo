f94c7cfea80cddde69881b5510dbec6f3f9aee16
Eeshan Kumar <e3.kumar@gmail.com>
2015-06-15 21:41:04 -0700

    add modularity

--- models/User.js
+++ models/User.js
@@ -3,10 +3,10 @@
 var mongoose = require('mongoose');
 
 var UserSchema = mongoose.Schema({
-  eat:          { type: Number                              },
-  access_token: {type: String                               },
-  facebook_id:  { type: String, required: true, unique: true},
-  friends:      { type: Array                               }
+  eat:            { type: Number                               },
+  access_token:   { type: String                               },
+  facebook_id:    { type: String, required: true, unique: true },
+  fb_last_update: { type: Date,                                }
 });
 
 module.exports = mongoose.model('User', UserSchema);
