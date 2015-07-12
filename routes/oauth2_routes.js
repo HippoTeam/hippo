0e8fc98a3744f13f943e24bf61a2cad5cd782cbe
clintonjnelson <clintonjnelson@live.com>
2015-07-12 16:14:38 -0700

    add each dev account for login via about_us images; hide facebook ids in env var references

--- routes/oauth2_routes.js
+++ routes/oauth2_routes.js
@@ -7,7 +7,7 @@ module.exports = function(app, passport) {
   app.use(bodyparser.json());
 
   // Facebook
-  app.get('/auth/facebook',
+  app.get('/auth/facebook/:name?',
     //Replace next two lines with commented lines below to remove backdoor
     userBackdoor,
     successMiddleware
@@ -50,9 +50,19 @@ module.exports = function(app, passport) {
 
   //Backdoor to allow anyone to use app
   function userBackdoor(req, res, next) {
+    var username = (req.params.name ? req.params.name.toLowerCase() : 'kumar'); // uppercase or default
+    var facebookId = {
+      jowell: process.env.JOWELL,
+      lamson: process.env.LAMSON,
+      kumar:  process.env.KUMAR,
+      clint:  process.env.CLINT
+    };
+
     //Find my user and send that back
-    User.findOne({facebook_id: '10105125701719480'}, function(err, user) {
-      if (err) return done('database error');
+    User.findOne({facebook_id: facebookId[username] }, function(err, user) {
+    // User.findOne({ facebook_id: '10104208393961668' }, function(err, user) {
+      if (err  ) { return done('database error'); }
+      if (!user) { return res.status(401).json({error: true, msg: 'user not found'})};
       //If user is already in the database return the user
       if (user && user.fb_last_update) {
         req.user = user;
